2018f60a094a6b88025e2885e5ef18e1135b01d0
Co0kei <thomasfox111@gmail.com>
2022-03-08 22:28:50 +0000

    Initial commit
    Set up cog system. Created basic interactions such as context menus and slash commands.

--- bot.py
+++ bot.py
@@ -1,19 +1,100 @@
-#Discord imports
+# Discord imports
+import json
+# Other imports
+import logging
+import sys
+import traceback
+
 import discord
+from discord import app_commands
 from discord.ext import commands
 
-#Other imports
-import json
+# Setup logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s',
+    datefmt='%I:%M:%S %p')
 
+log = logging.getLogger(__name__)
 
 # Define bot
-bot = commands.Bot(command_prefix = ['-'], reconnect=True, case_insensitive=True, intents = discord.Intents.all())
-bot.remove_command('help')
+dev_server_id = 759418498228158465
+
+bot = commands.Bot(command_prefix=['-'], description="A discord bot!", owner_id=452187819738267687,
+                   case_insensitive=True,
+                   allowed_mentions=discord.AllowedMentions(roles=False, everyone=False, users=True,
+                                                            replied_user=False),
+                   intents=discord.Intents.all(), help_command=None)
+tree = app_commands.CommandTree(bot)
+
+
+# Interactions
+@tree.context_menu(name='Report User', guild=discord.Object(id=dev_server_id))
+async def reportUser(interaction: discord.Interaction, member: discord.Member):
+    await interaction.response.send_message('Done', ephemeral=True)
+
+
+@tree.context_menu(name='Report Message', guild=discord.Object(id=dev_server_id))
+async def reportMessage(interaction: discord.Interaction, message: discord.Message):
+    if not message.content:
+        await interaction.response.send_message('No content!', ephemeral=True)
+        return
+
+    await interaction.response.send_message("Done", ephemeral=True)
+
+
+@tree.command(name='report', guild=discord.Object(id=dev_server_id))
+async def reportCommand(interaction: discord.Interaction, member: discord.Member, reason: str):
+    await interaction.response.send_message(f'{member=} {reason=}', ephemeral=True)
+
+
+# A command
+@commands.is_owner()
+@bot.command()
+async def sync(ctx):
+    await tree.sync(guild=discord.Object(id=759418498228158465))
+    await ctx.message.reply("done")
+
+
+# Events
 
 @bot.event
 async def on_ready():
-    print('Bot online!')
+    log.info('Bot online!')
+    log.info(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    log.info(f'Connected to {(len(bot.guilds))} Discord Servers.')
 
+    bot.uptime = discord.utils.utcnow()
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.NoPrivateMessage):
+        await ctx.author.send('This command cannot be used in private messages.')
+    elif isinstance(error, commands.DisabledCommand):
+        await ctx.author.send('Sorry. This command is disabled and cannot be used.')
+    elif isinstance(error, commands.CommandInvokeError):
+        original = error.original
+        if not isinstance(original, discord.HTTPException):
+            print(f'Error in {ctx.command.qualified_name}:', file=sys.stderr)
+            traceback.print_tb(original.__traceback__)
+            print(f'{original.__class__.__name__}: {original}', file=sys.stderr)
+    elif isinstance(error, commands.ArgumentParsingError):
+        await ctx.send(error)
+
+
+# Load extensions
+initial_extensions = (
+    'cogs.owner',
+    'cogs.other'
+)
+
+for extension in initial_extensions:
+    try:
+        bot.load_extension(extension)
+    except Exception as e:
+        print(f'Failed to load extension {extension}.', file=sys.stderr)
+        traceback.print_exc()
 
 # Retrieve token and start bot
 with open('config.json', 'r') as f:
---
+++ cogs/owner.py
@@ -0,0 +1,149 @@
+import logging
+import os
+import traceback
+
+import discord
+from discord.ext import commands
+
+log = logging.getLogger(__name__)
+
+
+class OwnerCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        log.info("loaded")
+
+    @commands.is_owner()
+    @commands.command()
+    async def cogs(self, ctx):
+        """ Command to list all cogs """
+        embed = discord.Embed(title=f"**Cogs**", colour=discord.Colour.blue())
+
+        cogs_data = ""
+        for filename in os.listdir('./cogs'):
+            if filename.endswith('.py'):
+                try:
+                    self.bot.load_extension(f'cogs.{filename[:-3]}')
+                except commands.ExtensionAlreadyLoaded:
+                    cogs_data += f"<:online:821068743987429438> {filename}\n"
+                    # loaded
+                else:
+                    self.bot.unload_extension(f'cogs.{filename[:-3]}')
+                    cogs_data += f"<:offline:821068938036379679> {filename}\n"
+                    # unloaded
+
+        embed.add_field(name="**Cogs**", value=f"{cogs_data}", inline=True)
+        await ctx.send(embed=embed)
+
+    @commands.is_owner()
+    @commands.command()
+    async def load(self, ctx, *, module=None):
+        """ Load a specific cog """
+        if module is None:
+            await ctx.send(f"\U0000274c Enter a cog to load!")
+            return
+        try:
+            self.bot.load_extension(f'cogs.{module}')
+            await ctx.send(f"\U00002705 Cog {module} loaded successfully!")
+
+        except commands.ExtensionAlreadyLoaded:
+            await ctx.send("\U0000274c This cog is already loaded!")
+
+        except commands.ExtensionNotFound:
+            await ctx.send("\U0000274c This cog could not be found!")
+
+        except Exception as e:
+            traceback.print_exc()
+            await ctx.send(f"\U0000274c {e}")
+
+    @commands.is_owner()
+    @commands.command()
+    async def unload(self, ctx, *, module=None):
+        """ Unload a specific cog """
+        if module is None:
+            await ctx.send(f"\U0000274c Enter a cog to unload!")
+            return
+        try:
+            self.bot.unload_extension(f'cogs.{module}')
+            await ctx.send(f"\U00002705 Cog {module} unloaded successfully!")
+
+        except commands.ExtensionNotLoaded:
+            await ctx.send("\U0000274c This cog is already unloaded!")
+
+        except commands.ExtensionNotFound:
+            await ctx.send("\U0000274c This cog could not be found!")
+
+        except Exception as e:
+            traceback.print_exc()
+            await ctx.send(f"\U0000274c {e}")
+
+    @commands.is_owner()
+    @commands.command(aliases=["r"])
+    async def reload(self, ctx, *, module=None):
+        """" Reload a specific cog """
+        if module is None:
+            await ctx.send(f"\U0000274c Enter a cog to reload!")
+            return
+        try:
+            self.bot.reload_extension(f"cogs.{module}")
+            await ctx.send(f"\U00002705 Successfully reloaded {module}!")
+
+        except commands.ExtensionNotFound:
+            await ctx.send("\U0000274c This cog could not be found!")
+
+        except Exception as e:
+            traceback.print_exc()
+            await ctx.send(f"\U0000274c {e}")
+
+    @commands.command()
+    async def about(self, ctx):
+        """Tells you information about the bot itself """
+
+        embed = discord.Embed(description='Latest Changes:\n')
+        embed.title = 'Official Bot Server Invite'
+        embed.url = 'https://discord.gg/asd'
+        embed.colour = discord.Colour.blurple()
+
+        owner = self.bot.get_user(self.bot.owner_id)
+        embed.set_author(name=str(owner), icon_url=owner.display_avatar.url)
+
+        # statistics
+        total_members = 0
+        total_unique = len(self.bot.users)
+
+        text = 0
+        voice = 0
+        guilds = 0
+        for guild in self.bot.guilds:
+            guilds += 1
+            if guild.unavailable:
+                continue
+
+            total_members += guild.member_count
+            for channel in guild.channels:
+                if isinstance(channel, discord.TextChannel):
+                    text += 1
+                elif isinstance(channel, discord.VoiceChannel):
+                    voice += 1
+
+        embed.add_field(name='Members', value=f'{total_members} total\n{total_unique} unique')
+        embed.add_field(name='Channels', value=f'{text + voice} total\n{text} text\n{voice} voice')
+
+        #  memory_usage = self.process.memory_full_info().uss / 1024**2
+        # cpu_usage = self.process.cpu_percent() / psutil.cpu_count()
+        # embed.add_field(name='Process', value=f'{memory_usage:.2f} MiB\n{cpu_usage:.2f}% CPU')
+
+        #   version = pkg_resources.get_distribution('discord.py').version
+        embed.add_field(name='Guilds', value=guilds)
+        # embed.add_field(name='Commands Run', value=sum(self.bot.command_stats.values()))
+        # embed.add_field(name='Uptime', value=self.get_bot_uptime(brief=True))
+        #   embed.set_footer(text=f'Made with discord.py v{version}', icon_url='http://i.imgur.com/5BFecvA.png')
+        embed.timestamp = discord.utils.utcnow()
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(OwnerCog(bot))
