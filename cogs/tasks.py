4f3fdd2635d9eefe7ccc1d21e4f3901dd976e92f
Co0kei <thomasfox111@gmail.com>
2022-03-11 21:47:16 +0000

    Small changes to also save guild data

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -12,29 +12,28 @@ class TaskCog(commands.Cog):
         self.updater.start()
 
     def cog_unload(self):
-        self.updater.cancel()
+        if self.updater.is_running():
+            self.updater.cancel()
 
-    @tasks.loop(minutes=5.0)
+    @tasks.loop(minutes=1.0)
     async def updater(self):
-        with open('stats.json', 'r', encoding='utf-8') as f:
-            data = json.load(f)
-            f.close()
-
-        data["commands_used"] = self.bot.commands_used
-
+        # save commands used
+        data = {"commands_used": self.bot.commands_used}
         with open('stats.json', 'w', encoding='utf-8') as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
             f.close()
 
-        log.info("Saved commands_used: " + str(self.bot.commands_used))
+        # log.info("[TASK] Saved commands_used: " + str(self.bot.commands_used))
+
+        # save guild settings
+        with open('guild_settings.json', 'w', encoding='utf-8') as f:
+            json.dump(self.bot.guild_settings, f, ensure_ascii=False, indent=4)
+            f.close()
+
+        # log.info("[TASk] Saved guild_settings: " + str(self.bot.guild_settings))
 
     @updater.before_loop
-    async def before_printer(self):
-        with open('stats.json', 'r', encoding='utf-8') as f:
-            data = json.load(f)
-            self.bot.commands_used = data["commands_used"]  # load command usage
-        f.close()
-
+    async def before_updater(self):
         await self.bot.wait_until_ready()
 
 
