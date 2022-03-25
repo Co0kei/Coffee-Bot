bcf0ec113d8e5c4bbc4ec11128679494599fd1f5
Co0kei <thomasfox111@gmail.com>
2022-03-25 11:58:38 +0000

    Moved bot settings to a Python file instead of json

--- bot.py
+++ bot.py
@@ -10,6 +10,9 @@ import topgg
 from discord.ext import commands
 
 # Setup logging
+from constants import WEBHOOK_URL, TOKEN, PREFIX, DEV_BOT_TOKEN, DEV_PREFIX, DEV_SERVER_ID, TOPGG_PORT, TOPGG_TOKEN, \
+    TOPGG_URL, TOPGG_PASSWORD
+
 logging.basicConfig(
     level=logging.INFO,
     format='[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s',
@@ -35,29 +38,15 @@ logging.getLogger('discord.client').addFilter(LoggingFilter())
 
 log = logging.getLogger(__name__)
 
-with open('config.json', 'r') as f:
-    botsettings = json.load(f)
+operatingSys = sys.platform
+log.info("USING OPERATING SYS: " + operatingSys)
 
-    operatingSys = sys.platform
-    log.info("USING OPERATING SYS: " + operatingSys)
-
-    dev_server_id = botsettings["dev_server_id"]
-    webhook_id = botsettings["webhook_id"]
-    webhook_token = botsettings["webhook_token"]
-
-    topgg_token = botsettings["topgg_token"]
-    topgg_url = botsettings["topgg_url"]
-    topgg_password = botsettings["topgg_password"]
-    topgg_port = botsettings["topgg_port"]
-
-    if sys.platform == "win32":
-        token = botsettings["dev_bot_token"]  # development bot
-        prefix = botsettings["dev_prefix"]
-    else:
-        token = botsettings["token"]  # production bot
-        prefix = botsettings["prefix"]
-
-    f.close()
+if sys.platform == "win32":
+    token = DEV_BOT_TOKEN  # development bot
+    prefix = DEV_PREFIX
+else:
+    token = TOKEN  # production bot
+    prefix = PREFIX
 
 # Define bot
 bot = commands.Bot(command_prefix=[prefix], description="A discord bot!", owner_id=452187819738267687,
@@ -66,7 +55,6 @@ bot = commands.Bot(command_prefix=[prefix], description="A discord bot!", owner_
                                                             replied_user=False),
                    intents=discord.Intents.all(), help_command=None)
 tree = bot.tree
-bot.dev_server_id = dev_server_id
 
 with open('stats.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
@@ -90,7 +78,7 @@ async def globalReportMessage(interaction: discord.Interaction, message: discord
     await bot.get_cog("ReportCommand").handleMessageReport(interaction, message)
 
 
-@tree.context_menu(name='Dev - Report Message', guild=discord.Object(id=dev_server_id))
+@tree.context_menu(name='Dev - Report Message', guild=discord.Object(id=DEV_SERVER_ID))
 async def devReportMessage(interaction: discord.Interaction, message: discord.Message):
     await bot.get_cog("ReportCommand").handleMessageReport(interaction, message)
 
@@ -101,7 +89,7 @@ async def globalReportUser(interaction: discord.Interaction, member: discord.Mem
     await bot.get_cog("ReportCommand").handleUserReport(interaction, member, None)
 
 
-@tree.context_menu(name='Dev - Report User', guild=discord.Object(id=dev_server_id))
+@tree.context_menu(name='Dev - Report User', guild=discord.Object(id=DEV_SERVER_ID))
 async def devReportUser(interaction: discord.Interaction, member: discord.Member):
     await bot.get_cog("ReportCommand").handleUserReport(interaction, member, None)
 
@@ -117,16 +105,14 @@ async def on_ready():
 @bot.event
 async def setup_hook():
     log.info(f'Logged in as {bot.user} (ID: {bot.user.id})')
-
-    bot.hook = discord.Webhook.partial(webhook_id, webhook_token, session=aiohttp.ClientSession(loop=bot.loop))
-
+    bot.hook = discord.Webhook.from_url(WEBHOOK_URL, session=aiohttp.ClientSession(loop=bot.loop))
     bot.uptime = discord.utils.utcnow()
 
     if sys.platform != "win32":
         # topgg stuff - only run if on productions bot
-        bot.topggpy = topgg.DBLClient(bot, topgg_token, autopost=True, post_shard_count=True)
-        bot.topgg_webhook = topgg.WebhookManager(bot).dbl_webhook(topgg_url, topgg_password)
-        await bot.topgg_webhook.run(topgg_port)
+        bot.topggpy = topgg.DBLClient(bot, TOPGG_TOKEN)
+        bot.topgg_webhook = topgg.WebhookManager(bot).dbl_webhook(TOPGG_URL, TOPGG_PASSWORD)
+        await bot.topgg_webhook.run(TOPGG_PORT)
 
     await load_cogs()
 
