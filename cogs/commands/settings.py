65a5d813a2eddf84ed71ed39feded5e970657fec
Co0kei <thomasfox111@gmail.com>
2022-04-13 21:47:48 +0100

    New link filter feature with optional whitelisted links. See /settings to configure.

--- cogs/commands/settings.py
+++ cogs/commands/settings.py
@@ -21,7 +21,6 @@ class SettingsCommand(commands.Cog):
             reportsChannel = reportsChannel[1:]
 
         reportsChannel = reportsChannel.replace(' ', '-')
-        # print(reportsChannel)
         channel = None
         for textChannel in guild.text_channels:
             if textChannel.name == reportsChannel.lower() or str(textChannel.id) == reportsChannel:
@@ -74,6 +73,14 @@ class SettingsCommand(commands.Cog):
                 invite_filter = self.bot.guild_settings[str(guild.id)]["invite_filter"]
         return invite_filter
 
+    def isLinkFilterEnabled(self, guild: discord.Guild) -> bool:
+        link_filter = False  # filter disabled by default
+
+        if str(guild.id) in self.bot.guild_settings:
+            if "link_filter" in self.bot.guild_settings[str(guild.id)]:
+                link_filter = self.bot.guild_settings[str(guild.id)]["link_filter"]
+        return link_filter
+
     def getSettingsEmbed(self, guild: discord.Guild) -> discord.Embed:
 
         reports_channel = "`None`"
@@ -85,6 +92,8 @@ class SettingsCommand(commands.Cog):
         report_admins = self.isReportAdminsEnabled(guild)
 
         invite_filter = self.isInviteFilterEnabled(guild)
+        link_filter = self.isLinkFilterEnabled(guild)
+        whitelisted_links = "`None`"
         mod_log_channel = "`None`"
 
         if str(guild.id) in self.bot.guild_settings:
@@ -118,6 +127,13 @@ class SettingsCommand(commands.Cog):
                 else:
                     mod_log_channel = mod_log_channel.mention
 
+            if "whitelisted_links" in self.bot.guild_settings[str(guild.id)]:
+                whitelisted_links = self.bot.guild_settings[str(guild.id)]["whitelisted_links"]
+                if whitelisted_links:  # if not empty
+                    whitelisted_links = ", ".join(whitelisted_links)
+                else:
+                    whitelisted_links = "`None`"
+
         embed = discord.Embed(title="Settings",
                               description=f'Click a button to edit the value.')
 
@@ -134,8 +150,14 @@ class SettingsCommand(commands.Cog):
                         value=f"Allow members to report themselves? `{report_self}`\n"
                               f"Allow members to report bots? `{report_bots}`\n"
                               f"Allow members to report server admins? `{report_admins}`\n\n"
+
                               f"Discord Invite Filter: `{invite_filter}`\n"
-                              f"Mod Log Channel: {mod_log_channel}",
+                              f"Link Filter: `{link_filter}`\n"
+                              f"Whitelisted Links: `{whitelisted_links[0:1000]}`\n",
+                        inline=False)
+
+        embed.add_field(name="Mod Log Channel",
+                        value=f"Which channel should I send attempts to bypass the invite / link filter to?\nValue: {mod_log_channel}",
                         inline=False)
 
         embed.colour = discord.Colour(0x2F3136)
@@ -178,6 +200,12 @@ class SettingsCommand(commands.Cog):
 
             if item.label == "Invite Filter":
                 if self.isInviteFilterEnabled(interaction.guild):
+                    item.style = discord.ButtonStyle.green
+                else:
+                    item.style = discord.ButtonStyle.red
+
+            if item.label == "Link Filter":
+                if self.isLinkFilterEnabled(interaction.guild):
                     item.style = discord.ButtonStyle.green
                 else:
                     item.style = discord.ButtonStyle.red
@@ -334,7 +362,7 @@ class SettingsCommand(commands.Cog):
 
             else:
                 embed = discord.Embed(title="Invite Filter Enabled")
-                embed.description = "Only server administrators can post Discord server invites."
+                embed.description = "Only members with the `Manage Messages` permission can post Discord server invites."
                 embed.colour = discord.Colour.green()
                 button.style = discord.ButtonStyle.green
 
@@ -344,12 +372,44 @@ class SettingsCommand(commands.Cog):
             await interaction.response.send_message(embed=embed, ephemeral=True)
             # print(self.bot.guild_settings)
 
-        @discord.ui.button(label='Mod Log Channel', style=discord.ButtonStyle.green, row=2)
+        @discord.ui.button(label='Link Filter', row=2)
+        async def linkFilter(self, interaction: discord.Interaction, button: discord.ui.Button):
+            link_filter = False  # filter disabled by default
+
+            if str(interaction.guild.id) in self.bot.guild_settings:
+                if "link_filter" in self.bot.guild_settings[str(interaction.guild.id)]:
+                    link_filter = self.bot.guild_settings[str(interaction.guild.id)]["link_filter"]
+            else:
+                self.bot.guild_settings[str(interaction.guild.id)] = {}
+
+            if link_filter:
+                embed = discord.Embed(title="Link Filter Disabled")
+                embed.description = "Everyone can post any links."
+                embed.colour = discord.Colour.dark_red()
+                button.style = discord.ButtonStyle.red
+
+            else:
+                embed = discord.Embed(title="Link Filter Enabled")
+                embed.description = "Only members with the `Manage Messages` permission can post any links. Other members can only post whitelisted links."
+                embed.colour = discord.Colour.green()
+                button.style = discord.ButtonStyle.green
+
+            self.bot.guild_settings[str(interaction.guild.id)]["link_filter"] = not link_filter
+            await self.message.edit(view=self)
+            await self.reloadSettingsEmbed()
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+
+        @discord.ui.button(label='Whitelisted Links', style=discord.ButtonStyle.green, row=2)
+        async def whitelistedLinks(self, interaction: discord.Interaction, button: discord.ui.Button):
+            whitelistedLinkModel = self.settingCog.WhitelistedLinkModel(self.bot, self)
+            await interaction.response.send_modal(whitelistedLinkModel)
+
+        @discord.ui.button(label='Mod Log Channel', style=discord.ButtonStyle.green, row=3)
         async def modLogChannel(self, interaction: discord.Interaction, button: discord.ui.Button):
             modLogChannelModel = self.settingCog.ModLogChannelModel(self.bot, self)
             await interaction.response.send_modal(modLogChannelModel)
 
-        @discord.ui.button(label='Finish', style=discord.ButtonStyle.grey, row=2)
+        @discord.ui.button(label='Finish', style=discord.ButtonStyle.grey, row=3)
         async def finish(self, interaction: discord.Interaction, button: discord.ui.Button):
             await self.on_timeout()
             self.stop()
@@ -491,6 +551,92 @@ class SettingsCommand(commands.Cog):
 
             await interaction.response.send_message(embed=embed, ephemeral=True)
             # print(self.bot.guild_settings)
+
+    class WhitelistedLinkModel(ui.Modal, title="Whitelisted Links"):
+
+        def __init__(self, bot=None, settingButtons=None):
+            super().__init__()
+            self.bot = bot
+            self.settingsButtons = settingButtons
+
+        add = ui.TextInput(label='Add Link', style=discord.TextStyle.short,
+                           placeholder="Enter any links to add to the whitelist such as 'tenor.com'.",
+                           required=False, max_length=1000)
+        remove = ui.TextInput(label='Remove Link', style=discord.TextStyle.short,
+                              placeholder="Enter any links to remove from the whitelist.",
+                              required=False, max_length=1000)
+
+        async def on_submit(self, interaction: discord.Interaction):
+            add = self.add.value
+            remove = self.remove.value
+
+            if not add and not remove:
+                embed = discord.Embed(title="No links found",
+                                      description="Please enter some links to either remove or add to the whitelist.",
+                                      colour=discord.Colour.dark_red())
+            else:
+
+                if not str(interaction.guild.id) in self.bot.guild_settings:
+                    self.bot.guild_settings[str(interaction.guild.id)] = {}
+                    # self.bot.guild_settings[str(interaction.guild.id)]["whitelisted_links"] = []
+                    current_links = []
+                else:
+                    if "whitelisted_links" in self.bot.guild_settings[str(interaction.guild.id)]:
+                        current_links = self.bot.guild_settings[str(interaction.guild.id)]["whitelisted_links"]
+                    else:
+                        current_links = []
+
+                msg = []
+
+                if add:
+                    to_add = add.replace(" ", "")
+                    to_add = to_add.replace("'", "")
+                    to_add = to_add.replace("\"", "")
+                    to_add = to_add.lower()
+                    to_add = to_add.split(",")
+                    to_add = list(dict.fromkeys(to_add))  # remove duplicates
+
+                    LINKS_TO_ADD = []
+
+                    for link in to_add:
+                        if link not in current_links:
+                            LINKS_TO_ADD.append(link)
+                        else:
+                            msg.append(f'{link} is already in the whitelist.')
+
+                    for link in LINKS_TO_ADD:
+                        current_links.append(link)
+                        msg.append(f'Added {link}.')
+
+                if remove:
+                    to_remove = remove.replace(" ", "")
+                    to_remove = to_remove.replace("'", "")
+                    to_remove = to_remove.replace("\"", "")
+                    to_remove = to_remove.lower()
+                    to_remove = to_remove.split(",")
+                    to_remove = list(dict.fromkeys(to_remove))  # remove duplicates
+
+                    LINKS_TO_REMOVE = []
+
+                    for link in to_remove:
+                        if link in current_links:
+                            LINKS_TO_REMOVE.append(link)
+                        else:
+                            msg.append(f'{link} is not in the whitelist.')
+
+                    for link in LINKS_TO_REMOVE:
+                        current_links.remove(link)
+                        msg.append(f'Removed {link}.')
+
+                self.bot.guild_settings[str(interaction.guild.id)]["whitelisted_links"] = current_links
+
+                embed = discord.Embed(title="Link Whitelist Updated")
+                embed.description = '\n'.join(msg)
+                embed.colour = discord.Colour.green()
+
+                await self.settingsButtons.reloadSettingsEmbed()
+
+            await interaction.response.send_message(embed=embed, ephemeral=True)
 
     class ModLogChannelModel(ui.Modal, title="Mod Log Channel"):
         """ The modal that asks you to enter a channel for reports to get sent to"""
---
+++ cogs/modules/linkfilter.py
@@ -0,0 +1,115 @@
+import logging
+import re
+from datetime import timedelta
+
+import discord
+from discord import NotFound
+from discord.ext import commands
+
+log = logging.getLogger(__name__)
+
+
+class LinkFilterCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    def getModLogChannel(self, guild: discord.Guild) -> discord.TextChannel:
+        mod_log_channel = None
+        if str(guild.id) in self.bot.guild_settings:
+            if "mod_log_channel" in self.bot.guild_settings[str(guild.id)]:
+                mod_log_channel = guild.get_channel(self.bot.guild_settings[str(guild.id)]["mod_log_channel"])
+        return mod_log_channel
+
+    def getWhitelistedLinks(self, guild: discord.Guild) -> list:
+        whitelisted_links = []
+        if str(guild.id) in self.bot.guild_settings:
+            if "whitelisted_links" in self.bot.guild_settings[str(guild.id)]:
+                whitelisted_links = self.bot.guild_settings[str(guild.id)]["whitelisted_links"]
+        return whitelisted_links
+
+    async def handleLink(self, message: discord.Message) -> bool:
+        content_lower = message.content.lower()
+
+        whitelisted_links = self.getWhitelistedLinks(message.guild)
+
+        for link in whitelisted_links:
+            content_lower = content_lower.replace(link, "")
+
+        if re.search("(https?://(?:www\\.|(?!www))[^\\s.]+\\.[^\\s]{2,}|www\\.[^\\s]+\\.[^\\s]{2,})", content_lower):
+            try:
+                await message.delete()
+            except NotFound:  # another bot deleted it
+                pass
+            try:
+                await message.author.timeout(timedelta(seconds=3))
+            except discord.Forbidden as e:  # no permission
+                pass
+            # log it
+            modLogChannel = self.getModLogChannel(message.guild)
+            if modLogChannel is not None:
+                embed = discord.Embed()
+                embed.set_author(name="Link Posted", icon_url=message.author.display_avatar.url)
+                embed.colour = discord.Colour(0x2F3136)
+                embedDescription = f'{message.author.mention} ({message.author}) tried to post a link.\n\n'
+                embedDescription += f'**Message Content:**\n`{message.clean_content[0:2000]}`'  # only display first 2000 chars
+                embedDescription += f'\n\n**Message\'s Info:**\n' \
+                                    f'Message ID: `{message.id}`\n' \
+                                    f'Channel: {message.channel.mention}\n' \
+                                    f'Time: {discord.utils.format_dt(message.created_at, "F")} ({discord.utils.format_dt(message.created_at, "R")})\n' \
+                                    f'Attachments: `{len(message.attachments)}`'
+                if len(message.attachments) != 0:
+                    attachement1 = message.attachments[0]
+                    if attachement1.content_type.startswith("image"):
+                        embed.set_image(url=attachement1.url)
+                        embedDescription += f"\n\n**Message Image:**"
+                embed.description = embedDescription
+                await modLogChannel.send(embed=embed)
+            return True
+        else:
+            return False
+
+    async def handleLinkEdit(self, before, after) -> bool:
+        content_lower = after.content.lower()
+
+        whitelisted_links = self.getWhitelistedLinks(after.guild)
+
+        for link in whitelisted_links:
+            content_lower = content_lower.replace(link, "")
+
+        if re.search("(https?://(?:www\\.|(?!www))[^\\s.]+\\.[^\\s]{2,}|www\\.[^\\s]+\\.[^\\s]{2,})", content_lower):
+            try:
+                await after.delete()
+            except NotFound:  # another bot deleted it
+                pass
+            try:
+                await after.author.timeout(timedelta(seconds=3))
+            except discord.Forbidden as e:  # no permission
+                pass
+            # log it
+            modLogChannel = self.getModLogChannel(after.guild)
+            if modLogChannel is not None:
+                embed = discord.Embed()
+                embed.set_author(name="Link Posted", icon_url=after.author.display_avatar.url)
+                embed.colour = discord.Colour(0x2F3136)
+                embedDescription = f'{after.author.mention} ({after.author}) tried to edit a link into a message.\n\n'
+                embedDescription += f'**Message Before:**\n`{before.clean_content[0:1000]}`\n\n'  # only display first 1000 chars
+                embedDescription += f'**Message After:**\n`{after.clean_content[0:1000]}`'  # only display first 1000 chars
+                embedDescription += f'\n\n**Message\'s Info:**\n' \
+                                    f'Message ID: `{after.id}`\n' \
+                                    f'Channel: {after.channel.mention}\n' \
+                                    f'Time: {discord.utils.format_dt(after.created_at, "F")} ({discord.utils.format_dt(after.created_at, "R")})\n' \
+                                    f'Attachments: `{len(after.attachments)}`'
+                if len(after.attachments) != 0:
+                    attachement1 = after.attachments[0]
+                    if attachement1.content_type.startswith("image"):
+                        embed.set_image(url=attachement1.url)
+                        embedDescription += f"\n\n**Message Image:**"
+                embed.description = embedDescription
+                await modLogChannel.send(embed=embed)
+            return True
+        else:
+            return False
+
+
+async def setup(bot):
+    await bot.add_cog(LinkFilterCog(bot))
