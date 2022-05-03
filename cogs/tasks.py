872c00dc1b7ad17eaec84e7058cf98449003406c
Co0kei <thomasfox111@gmail.com>
2022-05-03 14:44:25 +0100

    Reworked vote reminders: when enabled you receive one 12 hours after last vote. Then each 2 hours later if still not voted.

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -16,9 +16,67 @@ class TaskCog(commands.Cog):
 
     async def cog_load(self):
         self.updater.start()
+        self.vote_reminder.start()
 
     async def cog_unload(self):
         self.updater.cancel()
+        self.vote_reminder.cancel()
+
+    async def check_first_vote_reminder(self):
+        """ Check if its been 12 hours since a vote. But not any longer."""
+        current_time: int = int(time.time())
+        for discord_id, last_vote in self.bot.stat_data["vote_reminders"].items():
+            difference = (current_time - last_vote)
+            # log.info(f'{discord_id} {last_vote} {difference}')
+            if 43200 < difference < 43260:  # num of seconds in 12 hours # 0 < difference < 60:  #
+                user = self.bot.get_user(int(discord_id)) or await self.bot.fetch_user(int(discord_id))
+                log.info(f"Attempting to send vote reminder to {user}")
+                if user:
+                    try:
+                        await user.send(
+                            f"Hey {user.mention}, it has been 12 hours since you last voted! You can now vote again at https://top.gg/bot/950765718209720360/vote !",
+                            suppress_embeds=True)
+                        log.info(f'Sent vote reminder to {user}!')
+                    except discord.HTTPException:
+                        pass
+
+    async def check_followup_vote_reminders(self):
+        """ Check if its been longer then 14 hours since someone has voted. This is called every 2 hours """
+        current_time: int = int(time.time())
+        for discord_id, last_vote in self.bot.stat_data["vote_reminders"].items():
+            difference = (current_time - last_vote)
+            # log.info(f'{discord_id} {last_vote} {difference}')
+            if difference > 50400:  # num of seconds in 14 hours - so reminders send every 2 hours after if you dont vote before that
+                user = self.bot.get_user(int(discord_id)) or await self.bot.fetch_user(int(discord_id))
+                log.info(f"Attempting to send vote reminder to {user}")
+                if user:
+                    try:
+                        await user.send(
+                            f"Hey {user.mention}, you last voted <t:{last_vote}:R>! You can vote again at https://top.gg/bot/950765718209720360/vote ! "
+                            f"(You can disable reminders on the /vote command)",
+                            suppress_embeds=True)
+                        log.info(f'Sent vote reminder to {user}!')
+                    except discord.HTTPException:
+                        pass
+
+    @tasks.loop(hours=2)
+    async def vote_reminder(self):
+        await self.check_followup_vote_reminders()
+
+    @vote_reminder.before_loop
+    async def before_vote_reminder(self):
+        await self.bot.wait_until_ready()
+
+    @vote_reminder.error
+    async def on_vote_reminder_error(self, *args: Any) -> None:
+        exception: Exception = args[-1]
+        log.error(f'Unhandled exception in internal background task vote_reminder')
+        traceback.print_exception(type(exception), exception, exception.__traceback__)
+
+        await asyncio.sleep(5)
+        log.info("Restarting task...")
+
+        self.vote_reminder.restart()
 
     @tasks.loop(minutes=1.0)
     async def updater(self):
@@ -37,40 +95,12 @@ class TaskCog(commands.Cog):
         # Do backup once per day
         if time_now.hour == 0 and time_now.minute == 0:
             cmd = self.bot.get_command("backup")
-            await cmd(None)
-            log.info("Completed data backup.")
+            if cmd:
+                await cmd(None)
+                log.info("Completed data backup.")
 
         # check if there are any vote reminders to send!
-        current_time: int = int(time.time())
-        if "vote_reminder" in self.bot.vote_data:
-            vote_reminders: list = self.bot.vote_data["vote_reminder"]
-            reminders_send = []  # list of people that recieved a reminder
-            if len(vote_reminders) != 0:
-                for discordID in vote_reminders:
-                    last_vote = self.bot.vote_data[str(discordID)]["last_vote"]
-                    difference = (current_time - last_vote)
-                    if difference > 43200:  # num of seconds in 12 hours
-                        # if been more than 12 hours - send a reminder
-                        user = self.bot.get_user(int(discordID))
-                        log.info(f"Attempting to send vote reminder. User: {user}")
-                        if user is None:
-                            # user not in cache so attempt to retrieve them
-                            user = await self.bot.fetch_user(int(discordID))
-                            log.info(f"User was none attempted an API call. User: {user}")
-
-                        if user is not None:
-                            try:
-                                await user.send(
-                                    f"Hey {user.mention}, it has been 12 hours since you set a vote reminder! You can now vote again at https://top.gg/bot/950765718209720360/vote !",
-                                    suppress_embeds=True)
-                                log.info(f'Sent vote reminder to {user}!')
-                            except discord.HTTPException:
-                                pass
-                        reminders_send.append(discordID)
-
-            # now remove people from list that have been send a reminder
-            for discordID in reminders_send:
-                vote_reminders.remove(discordID)
+        await self.check_first_vote_reminder()
 
     @updater.before_loop
     async def before_updater(self):
